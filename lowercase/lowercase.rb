62405524a7e86d0398804bef0881bf81df56ec0a
jonBarcus <barcus.j@gmail.com>
2014-08-19 19:30:52 -0400

    added comments

--- lowercase/lowercase.rb
+++ lowercase/lowercase.rb
@@ -1,5 +1,9 @@
+# this opens the test file
 my_file = File.open(ARGV.first, 'r')
 
+# this will run through each line and
+# run DOWNCASE on the string and output the
+# downcased string
 my_file.each do |line|
   puts line.downcase
 end
