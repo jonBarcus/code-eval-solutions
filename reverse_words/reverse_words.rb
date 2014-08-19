80e8e3a01036f5cd0a83d47db9289df6dc700d76
jonBarcus <barcus.j@gmail.com>
2014-08-19 18:02:30 -0400

    added comments to reverse_words.rb

--- reverse_words/reverse_words.rb
+++ reverse_words/reverse_words.rb
@@ -1,24 +1,28 @@
-require 'pry'
-
 # this opens the test file
-
 my_file = File.open(ARGV.first, 'r')
 
 my_file.each do |line|
   # splitting the line in to an array
   original_array = line.split
 
+  # creates a new array variable so we can
+  # add contents
   new_array = []
 
+  # this variable will take the length of the array
+  # for the purpose of looping through it
   count = original_array.length
 
+  # this loop will add elements to a new array
   while count > 0
     new_array << original_array.pop
     count -= 1
   end
 
+  # this will be the new string created from the new_array
   new_string = new_array.join(" ")
 
+  # outputs the string without the end space
   puts new_string.chomp
 
 end
