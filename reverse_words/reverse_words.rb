adc75affcac05a20ca01c603a4f9401f207527d9
jonBarcus <barcus.j@gmail.com>
2014-08-19 17:56:16 -0400

    added while loop to count down array length and POP array elements to new array

--- reverse_words/reverse_words.rb
+++ reverse_words/reverse_words.rb
@@ -5,6 +5,18 @@ require 'pry'
 my_file = File.open(ARGV.first, 'r')
 
 my_file.each do |line|
-  new_array = line.split
-  binding.pry
+  # splitting the line in to an array
+  original_array = line.split
+
+  new_array = []
+
+  count = original_array.length
+
+  while count > 0
+    new_array << original_array.pop
+    count -= 1
+    binding.pry
+  end
+
+
 end
